8c44ddca1199a619399aa4af0243e5bdb66fca95
Inosilens <pashin.andrey94@gmail.com>
2021-07-21 22:54:10 +0700

    small refactoring

--- src/scripts/services/getComments.js
+++ src/scripts/services/getComments.js
@@ -1,3 +1,5 @@
-export const getComments = (url) => {
-  return fetch(url).then((response) => response.json());
+export const getComments = () => {
+  return fetch("https://jsonplaceholder.typicode.com/comments").then(
+    (response) => response.json()
+  );
 };
